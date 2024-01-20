0a13774172654845bcc56cdd8416befc1d701dec
Rider-7 <147482241+Rider-7@users.noreply.github.com>
2024-01-20 20:29:23 +0800

    add font assets

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// Components being tested
+import Router from './components/Router/Router';
+import ButtonTests from './components/Button/Button.test';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Router />
   </React.StrictMode>,
-)
+);
