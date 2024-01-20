537ab2e18fb215cd01e3ac1b7a3e52c33f783d05
Rider-7 <147482241+Rider-7@users.noreply.github.com>
2024-01-20 20:30:38 +0800

    create Router component

---
+++ src/components/Router/Router.jsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from '../../pages/Home';
+
+export default function Router() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Home />,
+    },
+  ]);
+  return <RouterProvider router={router} />;
+}
