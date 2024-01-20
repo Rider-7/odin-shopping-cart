a3492f911f1cd4363bed08fd41d227b5856e6dc4
Rider-7 <147482241+Rider-7@users.noreply.github.com>
2024-01-20 20:30:29 +0800

    create Nav component

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,23 @@
+import { Link } from 'react-router-dom';
+
+import Button from '../Button/Button';
+import ButtonStyles from '../Button/Button.module.css';
+import cartSVG from '../../assets/icon/cart.svg';
+
+import styles from './Nav.module.css';
+
+export default function Nav() {
+  return (
+    <nav className={styles.nav}>
+      <Link className={styles.link} to="/">Home</Link>
+      <Link className={styles.link} to="shop">Shop</Link>
+      <Link className={styles.link} to="contact">Contact</Link>
+      <Button
+        icon={cartSVG}
+        alt="cart"
+        style={ButtonStyles.cart}
+        theme={ButtonStyles['pink-theme']}
+      />
+    </nav>
+  );
+}
