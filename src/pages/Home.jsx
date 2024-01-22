f0b39ebd7a45d315eb356e9139d3556697d1fdb5
Rider-7 <147482241+Rider-7@users.noreply.github.com>
2024-01-22 22:07:30 +0800

    finalise 'home' page scaffolding

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,8 @@ import Nav from '../components/Nav/Nav';
 import Button from '../components/Button/Button';
 import styles from './Home.module.css';
 
+import heroJPG from '../assets/image/hero.jpg';
+
 export default function Home() {
   return (
     <>
@@ -15,6 +17,7 @@ export default function Home() {
           <span className={`${styles.logo} ${styles.large}`}>Boutique</span>
           <div className={styles['button-menu']}><Button>GO SHOP</Button></div>
         </div>
+        <img className={styles.image} src={heroJPG} alt="" />
       </div>
     </>
   );
