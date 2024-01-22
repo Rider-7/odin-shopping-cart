3b132b0d6911c67151dde2bd38649d6d87a21452
Rider-7 <147482241+Rider-7@users.noreply.github.com>
2024-01-22 22:04:58 +0800

    rename 'cart' CSS class to 'number'

--- src/components/Button/Button.test.jsx
+++ src/components/Button/Button.test.jsx
@@ -6,7 +6,7 @@ import cartSVG from '../../assets/icon/cart.svg';
 export default function ButtonTests() {
   return (
     <>
-      <Button icon={cartSVG} theme={styles['pink-theme']} style={styles.cart}>3</Button>
+      <Button icon={cartSVG} theme={styles['pink-theme']} style={styles.number}>3</Button>
       <Button>text</Button>
       <Button>TEXT</Button>
       <Button style={styles.small}>text</Button>
