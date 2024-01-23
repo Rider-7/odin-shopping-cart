326828a093fe829527a2948d6182978c9d0719ce
Rider-7 <147482241+Rider-7@users.noreply.github.com>
2024-01-23 19:31:17 +0800

    fix routing issue: header now does not re-render

--- src/components/Router/Router.jsx
+++ src/components/Router/Router.jsx
@@ -1,17 +1,20 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Home from '../../pages/Home';
+import Home from '../../pages/Home/Home';
 import Shop from '../../pages/Shop';
+import Layout from '../../pages/Layout';
+// import Cart from '../../pages/Cart';
 
 export default function Router() {
   const router = createBrowserRouter([
     {
       path: '/',
-      element: <Home />,
+      element: <Layout />,
+      children: [
+        { path: '/', element: <Home /> },
+        { path: '/shop', element: <Shop /> },
+      ],
     },
-    {
-      path: '/shop',
-      element: <Shop />,
-    },
+
   ]);
   return <RouterProvider router={router} />;
 }
