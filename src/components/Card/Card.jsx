3f29b9aa4d150ed9e0a8ddad460ef38791f7461d
Rider-7 <147482241+Rider-7@users.noreply.github.com>
2024-01-23 15:04:16 +0800

    create card component

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,17 @@
+import styles from './Card.module.css';
+
+import testIMG from'../../assets/image/hero.jpg';
+
+export default function Card(props) {
+  const { theme, name, price } = props;
+
+  return (
+    <button type="button" className={`${styles.card} ${theme}`}>
+      <img className={styles.image} src={testIMG} alt="" />
+      <div className={styles.description}>
+        <div className={styles.name}>{name}</div>
+        <div className={styles.price}>{price}</div>
+      </div>
+    </button>
+  );
+}
