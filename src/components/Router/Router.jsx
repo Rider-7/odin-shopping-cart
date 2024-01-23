bb7815d9aebddb932acce8f91055e1bf91b2977f
Rider-7 <147482241+Rider-7@users.noreply.github.com>
2024-01-23 17:10:12 +0800

    add shop url path to Router

--- src/components/Router/Router.jsx
+++ src/components/Router/Router.jsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from '../../pages/Home';
+import Shop from '../../pages/Shop';
 
 export default function Router() {
   const router = createBrowserRouter([
     {
       path: '/',
       element: <Home />,
+    },
+    {
+      path: '/shop',
+      element: <Shop />,
     },
   ]);
   return <RouterProvider router={router} />;
