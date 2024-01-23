a3c136fd152502ed985191ec88b0b0b82377dbec
Rider-7 <147482241+Rider-7@users.noreply.github.com>
2024-01-23 19:41:32 +0800

    refactor section element from Layout to Shop

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -2,13 +2,10 @@ import { Outlet } from 'react-router-dom';
 import Nav from '../components/Nav/Nav';
 import styles from './Layout.module.css';
 
-export default function Template(props) {
-  const { section } = props;
-
+export default function Layout() {
   return (
     <>
       <Nav />
-      { section && <h1 className={styles.section}>{section}</h1>}
       <Outlet />
     </>
   );
--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -3,15 +3,18 @@ import styles from './Shop.module.css';
 
 export default function Shop() {
   return (
-    <div className={styles['grid-container']}>
-      <h2 className={styles['grid-title']}>SHOES</h2>
-      <div className={styles.grid}>
-        <Card theme={styles['default-theme']} name="Shoes" price="$12.49" />
-        <Card theme={styles['default-theme']} name="Shoes" price="$12.49" />
-        <Card theme={styles['default-theme']} name="Shoes" price="$12.49" />
-        <Card theme={styles['default-theme']} name="Shoes" price="$12.49" />
-        <Card theme={styles['default-theme']} name="Shoes" price="$12.49" />
+    <>
+      <h1 className={styles.section}>SHOP</h1>
+      <div className={styles['grid-container']}>
+        <h2 className={styles['grid-title']}>SHOES</h2>
+        <div className={styles.grid}>
+          <Card theme={styles['default-theme']} name="Shoes" price="$12.49" />
+          <Card theme={styles['default-theme']} name="Shoes" price="$12.49" />
+          <Card theme={styles['default-theme']} name="Shoes" price="$12.49" />
+          <Card theme={styles['default-theme']} name="Shoes" price="$12.49" />
+          <Card theme={styles['default-theme']} name="Shoes" price="$12.49" />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
