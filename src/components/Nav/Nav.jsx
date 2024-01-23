5c8637206fb0c50ec65f470b2545fce5ce123990
Rider-7 <147482241+Rider-7@users.noreply.github.com>
2024-01-23 17:09:14 +0800

    fix url link issue

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -10,8 +10,8 @@ export default function Nav() {
   return (
     <nav className={styles.nav}>
       <Link className={styles.link} to="/">Home</Link>
-      <Link className={styles.link} to="shop">Shop</Link>
-      <Link className={styles.link} to="contact">Contact</Link>
+      <Link className={styles.link} to="/shop">Shop</Link>
+      <Link className={styles.link} to="/contact">Contact</Link>
       <Button
         icon={cartSVG}
         alt="cart"
