6b6c99e9bb44ec4ca139ff30506e851ebf429e43
Rider-7 <147482241+Rider-7@users.noreply.github.com>
2024-01-24 17:57:18 +0800

    refactor Cart page component

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,28 +1,22 @@
 import styles from './Cart.module.css';
 import Button from '../../components/Button/Button';
+import ProductBox from '../../components/ProductBox/ProductBox';
+import Quantity from '../../components/Quantity/Quantity';
 
 export default function Cart() {
   return (
     <>
-      <h1 className={styles.section}>SHOP</h1>
+      <h1 className={styles.section}>CART</h1>
       <div className={styles.cart}>
         <div className={styles.list}>
-          <div className={styles.product}>
-            <div>Product Name</div>
-            <div>Quantity</div>
-            <div>Unit Price</div>
-            <div>Final Price</div>
+          <ProductBox name="Adidas" />
+          <ProductBox name="Air Jordans" />
+          <div className={styles.final}>
+            <Quantity name="FINAL" value="$899.99" />
           </div>
-          <div className={styles.product}>
-            <div>Product Name</div>
-            <div>Quantity</div>
-            <div>Unit Price</div>
-            <div>Final Price</div>
+          <div className={styles.button}>
+            <Button theme="pink-theme" modifier="small">CHECKOUT</Button>
           </div>
-        </div>
-        <div className={styles.checkout}>
-          <div>Total Price</div>
-          <Button theme="pink-theme" modifier="small">GO TO CHECKOUT</Button>
         </div>
       </div>
     </>
