ce3c183fc7d27d7ecfa7993cc935d4297bea7a93
Rider-7 <147482241+Rider-7@users.noreply.github.com>
2024-01-25 23:15:03 +0800

    change static values into variables in ProductBox

--- src/components/ProductBox/ProductBox.jsx
+++ src/components/ProductBox/ProductBox.jsx
@@ -3,21 +3,19 @@ import styles from './ProductBox.module.css';
 import Quantity from '../Quantity/Quantity';
 
 export default function ProductBox(props) {
-  const { name } = props;
+  const { name, price } = props;
+
+  const quantity = 3;
+  const amount = price * quantity;
   return (
     <div className={styles.box}>
       <h2 className={styles.name}>{name}</h2>
-      {/* <ul className={styles.list}>
-        <li><b>Detail 1: </b>Lorem ipsum dolor sit amet</li>
-        <li><b>Detail 2: </b>Consectetur adipiscing elit</li>
-        <li><b>Detail 3: </b>Sed do eiusmod tempor</li>
-      </ul> */}
       <div className={styles.quantities}>
         <div className={styles.left}>
-          <Quantity name="Quantity" value="3" />
-          <Quantity name="Unit Price" value="$49.99" />
+          <Quantity name="Quantity" />
+          <Quantity name="Unit Price" value={`$${price}`} />
         </div>
-        <Quantity name="Amount" value="$499.99" />
+        <Quantity name="Amount" value={`$${amount}`} />
       </div>
     </div>
   );
