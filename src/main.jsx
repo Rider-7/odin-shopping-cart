cb78d750b39fb93e2e4d798cc8d423e1c2d41b57
Rider-7 <147482241+Rider-7@users.noreply.github.com>
2024-01-26 20:00:15 +0800

    update React entry point

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-// Components being tested
 import Router from './components/Router/Router';
-import ButtonTests from './components/Button/Button.test';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
