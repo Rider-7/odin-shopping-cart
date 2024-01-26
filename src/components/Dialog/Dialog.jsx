0c0ef43ddb106b388046d345d36f0e25212f81f9
Rider-7 <147482241+Rider-7@users.noreply.github.com>
2024-01-26 11:18:30 +0800

    refactor Dialog component

--- src/components/Dialog/Dialog.jsx
+++ src/components/Dialog/Dialog.jsx
@@ -1,4 +1,5 @@
 import { useRef } from 'react';
+
 import styles from './Dialog.module.css';
 
 import ProductBox from '../ProductBox/ProductBox';
@@ -7,6 +8,7 @@ import Quantity from '../Quantity/Quantity';
 
 export default function Dialog(props) {
   const {
+    id,
     imgUrl,
     name,
     description,
@@ -15,31 +17,51 @@ export default function Dialog(props) {
 
   const dialog = useRef();
 
-  function close() {
+  function closeDialog() {
     if (dialog.current.open) dialog.current.close();
+  }
+
+  function addToCart() {
+
   }
 
   return (
     <dialog id="modal" className={styles.dialog} ref={dialog}>
       <div className={styles.flex}>
         <div className={styles.product}>
-          <div className={styles.screen}>
-            <img className={styles.image} src={imgUrl} alt="" />
-          </div>
-          <div className={styles.information}>
-            <h1 className={styles.name}>{name}</h1>
-            <p className={styles.description}>
-              {description}
-            </p>
-            <span className={styles.price}><Quantity value={`$${price}`} /></span>
-          </div>
+          <Screen imgUrl={imgUrl} />
+          <Information name={name} description={description} price={price} />
         </div>
         <ProductBox isInput name="Product Name" price={price} />
         <div className={styles.buttons}>
-          <Button theme="default-theme" modifier="small" onClick={close}>BACK</Button>
-          <Button theme="pink-theme" modifier="small">ADD TO CART</Button>
+          <Button theme="default-theme" modifier="small" onClick={closeDialog}>BACK</Button>
+          <Button theme="pink-theme" modifier="small" onClick={addToCart}>ADD TO CART</Button>
         </div>
       </div>
     </dialog>
+  );
+}
+
+function Information(props) {
+  const { name, description, price } = props;
+
+  return (
+    <div className={styles.information}>
+      <h1 className={styles.name}>{name}</h1>
+      <p className={styles.description}>
+        {description}
+      </p>
+      <span className={styles.price}><Quantity value={`$${price}`} /></span>
+    </div>
+  );
+}
+
+function Screen(props) {
+  const { imgUrl } = props;
+
+  return (
+    <div className={styles.screen}>
+      <img className={styles.image} src={imgUrl} alt="" />
+    </div>
   );
 }
