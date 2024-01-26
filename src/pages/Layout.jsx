e0b3a586737d791180e08e4a872e7937e51011d0
Rider-7 <147482241+Rider-7@users.noreply.github.com>
2024-01-26 17:06:29 +0800

    fix bug: cartId now persists between browser sessions

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -5,13 +5,16 @@ import Header from '../components/Header/Header';
 
 export default function Layout() {
   let cartInit = JSON.parse(localStorage.getItem('cart'));
+  let cartIdInit = JSON.parse(localStorage.getItem('cartId'));
   if (!cartInit) cartInit = [];
+  if (!cartIdInit) cartIdInit = 0;
 
   const [cart, setCart] = useState(cartInit);
-  const cartId = useRef(0);
+  const cartId = useRef(cartIdInit);
 
   useEffect(() => {
     localStorage.setItem('cart', JSON.stringify(cart));
+    localStorage.setItem('cartId', JSON.stringify(cartId.current));
   }, [cart]);
 
   return (
