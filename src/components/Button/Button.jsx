dbc83e3b021d6bd6cac1ed5f3d6e36d69e860220
Rider-7 <147482241+Rider-7@users.noreply.github.com>
2024-01-27 19:27:52 +0800

    restyled Home page and related components

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -3,44 +3,28 @@ import styles from './Button.module.css';
 
 export default function Button(props) {
   const {
-    modifier,
-    theme,
     icon,
     alt,
     children,
-    onClick,
-    value,
+    ...rest
   } = props;
 
   return (
-    <button
-      className={`${styles[modifier]} ${styles[theme]}`}
-      type="button"
-      onClick={onClick}
-      value={value}
-    >
-      {icon !== null && <img src={icon} alt={alt} />}
+    <button type="button" { ...rest }>
+      {icon !== null && <img className={styles.img} src={icon} alt={alt} />}
       {children !== null && children}
     </button>
   );
 }
 
 Button.propTypes = {
-  modifier: PropTypes.string,
-  theme: PropTypes.string,
   children: PropTypes.node,
   icon: PropTypes.string,
   alt: PropTypes.string,
-  onClick: PropTypes.func,
-  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
 };
 
 Button.defaultProps = {
-  modifier: 'button',
-  theme: 'default-theme',
   children: null,
   icon: null,
   alt: null,
-  onClick: null,
-  value: null,
 };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -9,8 +9,8 @@ export default function Home() {
   return (
     <div className={styles.hero}>
       <div className={styles.container}>
-        <span className={`${styles.logo} ${styles.large}`}>Boutique</span>
-        <div className={styles['button-menu']}><Link className={styles.link} to="/shop"><Button>GO SHOP</Button></Link></div>
+        <span className={styles.logo}>Boutique</span>
+        <div className={styles.menu}><Link className={styles.link} to="/shop"><Button className={styles.button}>GO SHOP</Button></Link></div>
       </div>
       <img className={styles.image} src={heroJPG} alt="" />
     </div>
