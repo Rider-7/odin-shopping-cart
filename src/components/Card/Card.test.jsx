002d1ec4b857b155e23f4d3bfb39f2d91fcfb04c
Rider-7 <147482241+Rider-7@users.noreply.github.com>
2024-01-30 13:35:18 +0800

    delete unused tests

--- src/components/Card/Card.test.jsx
+++
@@ -1,9 +0,0 @@
-import Card from './Card';
-import styles from './Card.module.css';
-
-export default function CardTests() {
-  return (
-    <Card theme={styles['default-theme']} name="Shoes" price="$12.49" />
-  );
-
-}
